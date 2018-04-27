d1ec9120d670fcc0233ed2000d1ea3d51c4edb28
Brandon Fuller <bjcfuller@uri.edu>
2018-04-27 15:55:31 -0400

    Fix code styles

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -9,37 +9,38 @@
  * Set up Talks post type and custom fields
  */
 function uri_modern_home_create_feature_post_type() {
-  register_post_type( 'feature',
-    array(
-        'label' => 'Feature',
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'capability_type' => 'post',
-        'hierarchical' => true,
-        'rewrite' => array('slug' => 'features'),
-        'query_var' => true,
-        'has_archive' => true,
-        'exclude_from_search' => false,
-        'supports' => array('title','editor','thumbnail','revisions'),
-        'taxonomies' => array('post_tag', 'category'),
-        'labels' => array(
-            'name' => __( 'Features' ),
-            'singular_name' => __( 'Feature' ),
-            'add_new'            => __( 'Add New', 'Feature' ),
-            'add_new_item'       => __( 'Add New Feature' ),
-            'edit_item'          => __( 'Edit Feature' ),
-            'new_item'           => __( 'New Feature' ),
-            'all_items'          => __( 'All Features' ),
-            'view_item'          => __( 'View Feature' ),
-            'search_items'       => __( 'Search features' ),
-            'not_found'          => __( 'No features found' ),
-            'not_found_in_trash' => __( 'No fetures found in the Trash' ),
-            'parent_item_colon'  => '',
-            'menu_name'          => 'Features'
-        ),
-        'menu_icon' => 'dashicons-star-filled',
-    )
+register_post_type(
+	   'feature',
+	array(
+		'label' => 'Feature',
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'capability_type' => 'post',
+		'hierarchical' => true,
+		'rewrite' => array( 'slug' => 'features' ),
+		'query_var' => true,
+		'has_archive' => true,
+		'exclude_from_search' => false,
+		'supports' => array( 'title', 'editor', 'thumbnail', 'revisions' ),
+		'taxonomies' => array( 'post_tag', 'category' ),
+		'labels' => array(
+			'name' => __( 'Features' ),
+			'singular_name' => __( 'Feature' ),
+			'add_new'            => __( 'Add New', 'Feature' ),
+			'add_new_item'       => __( 'Add New Feature' ),
+			'edit_item'          => __( 'Edit Feature' ),
+			'new_item'           => __( 'New Feature' ),
+			'all_items'          => __( 'All Features' ),
+			'view_item'          => __( 'View Feature' ),
+			'search_items'       => __( 'Search features' ),
+			'not_found'          => __( 'No features found' ),
+			'not_found_in_trash' => __( 'No fetures found in the Trash' ),
+			'parent_item_colon'  => '',
+			'menu_name'          => 'Features',
+		),
+		'menu_icon' => 'dashicons-star-filled',
+	)
   );
 }
 add_action( 'init', 'uri_modern_home_create_feature_post_type' );
@@ -73,20 +74,20 @@ if ( function_exists( 'register_field_group' ) ) {
 					'formatting' => 'html',
 					'maxlength' => '',
 				),
-                array (
-                    'key' => 'field_5ae37adc1dfcc',
-                    'label' => 'Template',
-                    'name' => 'template',
-                    'type' => 'select',
-                    'instructions' => 'Select the template to use.',
-                    'choices' => array (
-                        'default' => 'Default',
-                        'external' => 'Minimal Branding',
-                    ),
-                    'default_value' => 'default',
-                    'allow_null' => 0,
-                    'multiple' => 0,
-                ),
+				array(
+					'key' => 'field_5ae37adc1dfcc',
+					'label' => 'Template',
+					'name' => 'template',
+					'type' => 'select',
+					'instructions' => 'Select the template to use.',
+					'choices' => array(
+						'default' => 'Default',
+						'external' => 'Minimal Branding',
+					),
+					'default_value' => 'default',
+					'allow_null' => 0,
+					'multiple' => 0,
+				),
 			),
 			'location' => array(
 				array(
