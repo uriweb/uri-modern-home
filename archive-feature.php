d56e319319e1f2326d6061a1fdcfcf5eff075ed8
Brandon Fuller <bjcfuller@uri.edu>
2018-05-15 15:21:14 -0400

    Display feature archives as tiles of story cards

--- archive-feature.php
+++ archive-feature.php
@@ -19,19 +19,32 @@ get_header(); ?>
 			<h1 class="page-title">Features</h1>
 		</header><!-- .page-header -->
 
+		<div class="cl-tiles thirds">
 		<?php
 		/* Start the Loop */
 		while ( have_posts() ) :
 			the_post();
 
-			/*
-			 * Include the Post-Format-specific template for the content.
-			 * If you want to override this in a child theme, then include a file
-			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-			 */
-			get_template_part( 'template-parts/content-feature' );
+			if ( function_exists( 'uri_cl_shortcode_scard' ) ) {
+
+				echo do_shortcode( '[cl-scard excerpt=" " post="' . get_the_ID() . '"]' );
+
+			} else {
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content-feature' );
+
+			}
 
 		endwhile;
+
+		?>
+		</div>
+		<?php
 
 		the_posts_navigation();
 
