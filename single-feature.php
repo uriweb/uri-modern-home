177dbdbed6438bd8682f8a43357cb5ef1a7ad580
Brandon Fuller <bjcfuller@uri.edu>
2018-05-15 15:21:39 -0400

    Remove post navigation and comments from feature posts

--- single-feature.php
+++ single-feature.php
@@ -26,18 +26,6 @@ if ( get_field( 'stylesheet' ) ) : ?>
 
 			get_template_part( 'template-parts/content-feature' );
 
-			the_post_navigation(
-				array(
-					'prev_text' => 'Previous post',
-					'next_text' => 'Next post',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
 		endwhile; // End of the loop.
 		?>
 
