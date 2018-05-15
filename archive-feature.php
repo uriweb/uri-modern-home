375b2bd6c924274cfaac00e4348be29b8d1bbe6c
Brandon Fuller <bjcfuller@uri.edu>
2018-05-15 14:46:15 -0400

    Change feature archive page title; add support for hiding featured image to feature posts

--- archive-feature.php
+++ archive-feature.php
@@ -16,10 +16,7 @@ get_header(); ?>
 	?>
 
 		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-			?>
+			<h1 class="page-title">Features</h1>
 		</header><!-- .page-header -->
 
 		<?php
