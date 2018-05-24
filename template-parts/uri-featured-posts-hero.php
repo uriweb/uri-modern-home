ba937adeb12674a0b2389300d9ece36d53251c69
Brandon Fuller <bjcfuller@uri.edu>
2018-05-24 09:35:06 -0400

    Fix code styles

--- template-parts/uri-featured-posts-hero.php
+++ template-parts/uri-featured-posts-hero.php
@@ -1,22 +1,23 @@
 <?php
-/** 
+/**
  * Default template for Featured Posts widget
+ *
+ * @package uri-modern-home
  */
 
 	$html .= '<div>';
 
-	foreach($posts_array as $post) {
-		setup_postdata( $post );
+	foreach ( $posts_array as $post ) {
+	setup_postdata( $post );
 
-		$excerpt = get_field( 'hero_excerpt', $post->ID, FALSE );
+	$excerpt = get_field( 'hero_excerpt', $post->ID, false );
 
-		if( !empty( $excerpt ) ) {
-			$html .= $excerpt;
+	if ( ! empty( $excerpt ) ) {
+		$html .= $excerpt;
 		} else {
-			$html .= 'Hero excerpt is empty.  :(';
+		$html .= 'Hero excerpt is empty.  :(';
 		}
-
-	}
+}
 	wp_reset_postdata();
 	$html .= '</div>';
 
