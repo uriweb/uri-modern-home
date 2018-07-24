5cda4e76fb5dc2bedd8e15a5d3b91e3a7cae21a3
Brandon Fuller <bjcfuller@uri.edu>
2018-07-24 09:41:31 -0400

    Fix style errors

--- features/2018/the-internationalists.js
+++ features/2018/the-internationalists.js
@@ -1,47 +1,51 @@
 /**
  * THE INTERNATIONALISTS
+ *
+ * @package uri-modern-home
  */
 
 ( function() {
-	
+
 	'use strict';
-	
+
 	var els, l;
-	
-	window.addEventListener( 'load', function() {
-		
+
+	window.addEventListener(
+		 'load', function() {
+
 		var i;
-		
+
 		els = document.querySelectorAll( '.artboard.reveal' );
 		l = els.length;
-		
+
 		for ( i = 0; i < l; i++ ) {
-			els[i].classList.add( 'has-js' );
+				els[i].classList.add( 'has-js' );
 		}
-		
+
 		showImages;
-		
-	});
-	
+
+	}
+		);
+
 	window.addEventListener( 'scroll', showImages, false );
 	window.addEventListener( 'resize', showImages, false );
-	
+
 	function showImages() {
-		
+
 		var vh, offset, i;
-		
+
 		vh = window.innerHeight;
 
 		for ( i = 0; i < l; i++ ) {
-			
+
 			offset = els[i].getBoundingClientRect().top;
-									
+
 			if ( vh * .8 > offset ) {
 				els[i].classList.add( 'visible' );
 			}
-			
+
 		}
-		
+
 	}
-	
+
 })();
