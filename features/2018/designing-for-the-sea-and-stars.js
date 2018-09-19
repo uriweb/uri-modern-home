9ad796ad9d3228bb8c036a49db99fd58a2fae26c
Brandon Fuller <bjcfuller@uri.edu>
2018-09-19 11:00:30 -0400

    Fix code styles

--- features/2018/designing-for-the-sea-and-stars.js
+++ features/2018/designing-for-the-sea-and-stars.js
@@ -201,25 +201,26 @@
 
 		var outer, widthNoScroll, inner, widthWithScroll;
 
-		outer = document.createElement( "div" );
-		outer.style.visibility = "hidden";
-		outer.style.width = "100px";
-		outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+		outer = document.createElement( 'div' );
+		outer.style.visibility = 'hidden';
+		outer.style.width = '100px';
+		outer.style.msOverflowStyle = 'scrollbar'; // Needed for WinJS apps
 
 		document.body.appendChild( outer );
 
 		widthNoScroll = outer.offsetWidth;
-		// force scrollbars
-		outer.style.overflow = "scroll";
 
-		// add innerdiv
-		inner = document.createElement( "div" );
-		inner.style.width = "100%";
+		// Force scrollbars
+		outer.style.overflow = 'scroll';
+
+		// Add innerdiv
+		inner = document.createElement( 'div' );
+		inner.style.width = '100%';
 		outer.appendChild( inner );
 
 		widthWithScroll = inner.offsetWidth;
 
-		// remove divs
+		// Remove divs
 		outer.parentNode.removeChild( outer );
 
 		return widthNoScroll - widthWithScroll;
