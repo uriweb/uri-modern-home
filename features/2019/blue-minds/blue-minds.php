628a60919bd3d44b964186fa4d1ab4299596b754
Brandon Fuller <bjcfuller@uri.edu>
2019-10-02 14:07:25 -0400

    Create a shortcode and corresponding template for building Blue Minds feature story teasers

---
+++ features/2019/blue-minds/blue-minds.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Blue Minds
+ *
+ * @package uri-modern-home
+ */
+
+ /**
+  * Story block shortcode
+  *
+  * @param arr $atts the attributes.
+  * @param str $content the content.
+  */
+function uri_modern_features_blue_minds_story_block( $atts, $content = null ) {
+
+	// Attributes.
+	$atts = shortcode_atts(
+			array(
+				'wrapper_id' => '',
+				'post_id' => '',
+				'h1' => '',
+				'h2' => '',
+				'media_html' => '',
+			),
+		$atts
+		);
+
+	ob_start();
+
+  include 'story-block-template.php';
+
+	$output .= ob_get_clean();
+
+	return $output;
+
+}
+add_shortcode( 'uri-blue-minds-story-block', 'uri_modern_features_blue_minds_story_block' );
