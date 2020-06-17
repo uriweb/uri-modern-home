a61568a333692981450bd7b802e0c380491462f4
Brandon Fuller <bjcfuller@uri.edu>
2020-06-17 10:36:02 -0400

    Add mobile nav styles and sticky nav; needs work

--- features/2020/first-amendment/src/content.js
+++ features/2020/first-amendment/src/content.js
@@ -41,15 +41,47 @@
 
 	function navigation() {
 		const nav = document.getElementById( 'story-navigation' );
-		const links = nav.querySelectorAll( 'a' );
+		const panelsParent = document.getElementById( 'nav-panels' );
+		const links = panelsParent.querySelectorAll( 'a' );
+
+		data.navPos = nav.offsetTop + nav.offsetHeight;
 
 		for ( let i = 0; i < links.length; i++ ) {
 			setUpNavLink( links[ i ] );
 		}
 
-		const panels = nav.querySelectorAll( '.nav-panel' );
+		const panels = panelsParent.querySelectorAll( '.nav-panel' );
 		for ( let i = 0; i < panels.length; i++ ) {
-			setUpNavPanel( panels[ i ] );
+			setUpNavPanel( panels[ i ], nav );
+		}
+
+		const backbutton = document.createElement( 'div' );
+		backbutton.className = 'nav-back-button';
+		backbutton.innerHTML = 'Back';
+		backbutton.addEventListener( 'click', function() {
+			nav.setAttribute( 'data-active', '' );
+		}, false );
+		panelsParent.insertBefore( backbutton, panels[ 0 ] );
+
+		const menubar = document.createElement( 'div' );
+		menubar.id = 'nav-menubar';
+		menubar.innerHTML = 'Sections';
+		menubar.addEventListener( 'click', function() {
+			nav.classList.toggle( 'nav-open' );
+		}, false );
+		nav.insertBefore( menubar, panelsParent );
+
+		window.addEventListener( 'scroll', handleSticky.bind( null, nav ), false );
+		window.addEventListener( 'resize', handleSticky.bind( null, nav ), false );
+	}
+
+	function handleSticky( nav ) {
+		const y = window.pageYOffset;
+		if ( y >= data.navPos ) {
+			window.pageYOffset += nav.offsetHeight;
+			nav.classList.add( 'sticky' );
+		} else {
+			nav.classList.remove( 'sticky' );
 		}
 	}
 
@@ -67,14 +99,21 @@
 		}, false );
 	}
 
-	function setUpNavPanel( p ) {
+	function setUpNavPanel( p, nav ) {
 		const id = p.getAttribute( 'id' );
-		const section = document.getElementById( id.replace( 'nav-panel-', '' ) );
+		const name = id.replace( 'nav-panel-', '' );
+		const section = document.getElementById( name );
 
 		p.addEventListener( 'click', function( e ) {
-			if ( 'A' !== e.target.nodeName ) {
-				section.scrollIntoView( { behavior: 'smooth', block: 'start', inline: 'nearest' } );
-			}
+			widthSwitcher( e, section, nav, name );
 		}, false );
+	}
+
+	function widthSwitcher( e, section, nav, name ) {
+		if ( 600 > window.innerWidth ) {
+			nav.setAttribute( 'data-active', 'active-panel-' + name );
+		} else if ( 'A' !== e.target.nodeName ) {
+			section.scrollIntoView( { behavior: 'smooth', block: 'start', inline: 'nearest' } );
+		}
 	}
 }() );
