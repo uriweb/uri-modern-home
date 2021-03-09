0d202808629481876f566ddf5d98a324869a6ed7
Brandon Fuller <bjcfuller@uri.edu>
2021-03-09 11:01:05 -0500

    Adds animation to the hero (still very buggy)

--- inc/templates/generic-hero.php
+++ inc/templates/generic-hero.php
@@ -17,18 +17,41 @@ if ( ! empty( $atts['css'] ) ) {
 
 $output .= '>';
 
+// The backdrop
+$bg = '<div class="uri-generic-hero-backdrop">';
+$speeds = array(
+	'low',
+	'medium',
+	'high',
+);
+for ( $n = 0; $n <= 10; $n++ ) {
+	$w = rand( 2, 8 ) * 10;
+	$x = rand( 10, 90 );
+	$o = rand( 1, 5 ) / 10;
+	$s = $speeds[ array_rand( $speeds, 1 ) ];
+	$bg .= '<div class="speed-' . $s . '" style="width:calc( 1vw * ' . $w . '); left:' . ( $x - ( $w / 2 ) ) . '%; opacity:' . $o . '"></div>';
+}
+
+$bg .= '</div>';
+$output .= $bg;
+
 $output .= '<div class="uri-generic-hero-proper">';
+
+
 
 $output .= '<div class="uri-generic-hero-text">';
 
+// The title
 if ( ! empty( $atts['title'] ) ) {
 	$output .= '<h1>' . $atts['title'] . '</h1>';
 }
 
+// The body
 if ( ! empty( $atts['body'] ) ) {
 	$output .= '<p>' . $atts['body'] . '</p>';
 }
 
+// The button
 if ( ! empty( $atts['link'] ) ) {
 	$button = '<a class="uri-generic-hero-button" href="' . $atts['link'] . '" title="' . $atts['tooltip'] . '">' . $atts['button'] . '</a>';
 	if ( function_exists( 'uri_cl_shortcode_button' ) ) {
